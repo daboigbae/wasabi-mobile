fbccb46174c1c7e17257f4e91f9d3e41dd44f63a
Gabriel Higareda <ghigared@gmail.com>
2022-07-22 09:12:55 -0600

    Addressed name change for ios

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/temp/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/wasabimobile/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
