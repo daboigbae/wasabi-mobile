dfc177430c39195316396bbb509093030f05cdac
Gabriel Higareda <ghigared@gmail.com>
2022-07-22 08:01:38 -0600

    Added home screen to project

--- src/utils/screens.js
+++ src/utils/screens.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import { HOME_SCREENS } from "../utils/constants";
 import LibraryScreen from "../screens/LibraryScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 export const HOME_SCREENS_ARRAY = [
 	{
 		name: HOME_SCREENS.HOME_SCREEN,
-		component: () => <LibraryScreen />,
+		component: () => <HomeScreen />,
 		tabIcon: "home"
 	},
 	{
