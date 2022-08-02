64fea66297712426b1c7e661445bfbbb0d36c576
Manbeartrain <ahigareda15@gmail.com>
2022-08-02 13:31:02 -0700

    Completed user profile screen

--- src/screens/userProfileScreens/UserProfileScreen.js
+++ src/screens/userProfileScreens/UserProfileScreen.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import {
+	PixelRatio,
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View
+} from "react-native";
 import PropTypes from "prop-types";
 import GlobalStyles from "../../utils/GlobalStyles";
 import { COLOR_PALETTE } from "../../utils/constants";
 
 import UserProfileAvatar from "../../components/profile/UserProfileAvatar";
+import { useSelector } from "react-redux";
+import Button from "../../components/common/Button";
 
 const UserProfileScreen = () => {
+	const username = useSelector(
+		({ UserSlice }) => UserSlice?.userInformation?.username
+	);
+
 	return (
 		<SafeAreaView style={[StyleSheet.absoluteFill, GlobalStyles.appView]}>
 			<ScrollView style={styles.wrapper}>
 				<View style={styles.profileDetails}>
 					<UserProfileAvatar />
+					<Text style={styles.username}>{username || "Default User"}</Text>
+					<Button text="Edit profile" textStyle={styles.buttonText} />
 				</View>
 			</ScrollView>
 		</SafeAreaView>
@@ -35,7 +50,16 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center"
 	},
-	text: {
-		color: COLOR_PALETTE.white
+	username: {
+		color: COLOR_PALETTE.white,
+		fontSize: 32 * PixelRatio.getFontScale(),
+		fontWeight: "800",
+		textAlign: "center",
+		marginVertical: 16
+	},
+	buttonText: {
+		color: COLOR_PALETTE.white,
+		fontSize: 16 * PixelRatio.getFontScale(),
+		fontWeight: "800"
 	}
 });
